cd3cb20a0f63106155724496a42bea06a66cf948
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-04-11 20:12:54 -0300

    Componentes

---
+++ 3 - Componentes/index.jsx
@@ -0,0 +1,15 @@
+
+function Button() {
+    return <button className="btn btn-outline-dange>Butão Comprar</button>
+}
+
+function Lista() {
+    const lista = (
+        <ul>
+            <li>Primeiro</li>
+            <li>Segundo</li>
+            <li>Terceiro</li>
+        </ul>
+    )
+    return lista
+}
