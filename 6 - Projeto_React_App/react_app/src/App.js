4d1f59fcedccf27eb026015bd73cd61949c5e3cd
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-04-16 12:49:06 -0300

    components

--- 6 - Projeto_React_App/react_app/src/App.js
+++ 6 - Projeto_React_App/react_app/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from "./Logo";
+import HelloWorld from "./Hello";
 
-function App() {
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <HelloWorld />
     </div>
   );
 }
-
-export default App;
---
+++ 6 - Projeto_React_App/react_app/src/Hello.js
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>Este é um projeto react_app, onde eu aprendo a utilizar o create-react-app, e tambem modifico para praticar os components</p>
+        </div>
+    )
+}
