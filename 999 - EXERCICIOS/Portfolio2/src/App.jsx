27974a9507f93671e5be9c77a97df4d948f9ec18
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-04-20 12:45:29 -0300

    problemas com onclick em React mudar para onClick={}

--- 999 - EXERCICIOS/Portfolio2/src/App.jsx
+++ 999 - EXERCICIOS/Portfolio2/src/App.jsx
@@ -1,10 +1,14 @@
 import React, {Fragment} from 'react'
 import Header from './components/header/Header.jsx'
+import SobreMim from './components/main/sobreMim/SobreMim.jsx'
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <SobreMim />
+      </main>
     </Fragment>
   )
 }
