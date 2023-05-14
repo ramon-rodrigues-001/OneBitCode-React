816d78cf8c89f7cba34734d44f2dcbdcea947496
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-05-14 20:27:09 -0300

    Lidando com projeto

--- 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/App.jsx
+++ 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/App.jsx
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react'
+import Header from './components/1-Header/Header.jsx'
+import Apresentation from './components/2-Apresentation/Apresentation.jsx'
 
 function App() {
-
   return (
-    <h1>Ola, Mundo</h1>
+    <Fragment>
+      <Header />
+      <Apresentation />
+    </Fragment>
   )
 }
 
---
+++ 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/components/1-Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+
+    function ativarExtensao() {
+        if () {
+            
+        }
+    }
+
+    return (
+        <header id="header">
+            <nav id='nav'>
+                <h2>Logo</h2>
+            </nav>
+            <div id='extensao_nav' onScroll={ativarExtensao}>
+                
+            </div>
+        </header>
+    )
+}
