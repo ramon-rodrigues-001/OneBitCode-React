af4f05b38dd90482c38f0eda788c9bd3598133fb
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-05-15 19:35:06 -0300

    Fim do dia

--- 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/components/3-CardFlutuante/CardsFlutuante.jsx
+++ 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/components/3-CardFlutuante/CardsFlutuante.jsx
@@ -4,8 +4,8 @@ export default function CardsFlutuante() {
     return (
         <div id="CardsFlutuante">
             <div className='cards' id='card-1'>
-                <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur</p>
+                <h2>COLEÇÕES</h2>
+                <p>COLEÇÕES EXCLUSIVAS</p>
             </div>
             <div className='cards' id='card-2'></div>
             <div className='cards' id='card-3'></div>
