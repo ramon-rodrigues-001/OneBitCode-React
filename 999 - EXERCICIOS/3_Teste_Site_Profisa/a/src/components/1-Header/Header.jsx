5841ace0d5d64c1a80df1e6ce548841ff4bbd7e5
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-05-14 21:14:55 -0300

    Ainda com muitas duvidas

--- 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/components/1-Header/Header.jsx
+++ 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/components/1-Header/Header.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(Props) {
 
-    function ativarExtensao() {
-        if () {
-            
+    window.onscroll = ()=> {
+        const scroll = document.documentElement.scrollTop
+        
+        
+        if (scroll == "0") {
+            return (
+                <header id="header">
+                    <nav id='nav'>
+                        <h2>Logo</h2>
+                    </nav>
+                    <div id='extensao_nav'>
+                        
+                    </div>
+                </header>
+            )
+        }
+        else {
+            return (
+                <header id="header">
+                    <nav id='nav'>
+                        <h2>Logo</h2>
+                    </nav>
+                </header>
+            )
         }
     }
 
-    return (
-        <header id="header">
-            <nav id='nav'>
-                <h2>Logo</h2>
-            </nav>
-            <div id='extensao_nav' onScroll={ativarExtensao}>
-                
-            </div>
-        </header>
-    )
+    
 }
