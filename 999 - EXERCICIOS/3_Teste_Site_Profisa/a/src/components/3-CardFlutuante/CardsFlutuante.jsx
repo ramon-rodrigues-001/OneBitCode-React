73f06ff1e2f1a019fe4c766f5bfbc662024fb029
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-05-16 18:21:07 -0300

    continuando com projeto floricultura

--- 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/components/3-CardFlutuante/CardsFlutuante.jsx
+++ 999 - EXERCICIOS/3_Teste_Site_Profisa/a/src/components/3-CardFlutuante/CardsFlutuante.jsx
@@ -4,8 +4,16 @@ export default function CardsFlutuante() {
     return (
         <div id="CardsFlutuante">
             <div className='cards' id='card-1'>
-                <h2>COLEÇÕES</h2>
-                <p>COLEÇÕES EXCLUSIVAS</p>
+                <div id='icone'>
+                    <img src="./src/assets/icon-check.webp" alt="Icon Check" id='img-check' />
+                    <div>
+                        <h2>100% SEGURO</h2>
+                        <p>Segurança Nas Entregas</p>
+                    </div>
+                </div>
+                <div id='text-card'>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, voluptates? Eum ea labore, culpa totam dolores cupiditate non blanditiis, consequatur recusandae ipsam at molestias perferendis, distinctio eius. Quod, ea quasi?</p>
+                </div>
             </div>
             <div className='cards' id='card-2'></div>
             <div className='cards' id='card-3'></div>
