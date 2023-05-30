f84fb75b807b72830620b29f968a79f9a878ad14
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-05-29 21:37:59 -0300

    Incrementando API

--- 999 - EXERCICIOS/1-Teste-Conhecimento-Vite/vite-project/src/components/Planetas/index.jsx
+++ 999 - EXERCICIOS/1-Teste-Conhecimento-Vite/vite-project/src/components/Planetas/index.jsx
@@ -2,33 +2,39 @@ import React, { Component, Fragment } from "react"
 import "./index.css"
 import CadaPlaneta from "./cada-planeta/CadaPlaneta"
 
+
+async function getPlanets() {
+  const response = await fetch ('http://127.0.0.1:5173/API/planets.json')
+  const data = response.json()
+  return data
+}
+
+
 export default class Planetas extends Component {
     constructor(props) {
         super(props) 
         this.state = {
             Planets: [
-              {
-                id: 'Mercurio',
-                Title: 'Mercurio', 
-                Img: "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
-                Paragrafh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, obcaecati velit consequatur doloremque qui asperiores distinctio culpa ducimus recusandae. Facilis aliquid sequi amet. Pariatur eveniet molestiae exercitationem voluptate eligendi fuga.",
-                Link: "https://pt.wikipedia.org/wiki/Marte_(planeta)"
-              },{
-                id: 'Plutão',
-                Title: 'Plutão', 
-                Img: "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
-                Paragrafh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, obcaecati velit consequatur doloremque qui asperiores distinctio culpa ducimus recusandae. Facilis aliquid sequi amet. Pariatur eveniet molestiae exercitationem voluptate eligendi fuga.",
-                Link: "https://pt.wikipedia.org/wiki/Marte_(planeta)"
-              },{
-                id: 'Marte',
-                Title: 'Marte', 
-                // Img: "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
-                Paragrafh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, obcaecati velit consequatur doloremque qui asperiores distinctio culpa ducimus recusandae. Facilis aliquid sequi amet. Pariatur eveniet molestiae exercitationem voluptate eligendi fuga.",
-                Link: "https://pt.wikipedia.org/wiki/Marte_(planeta)"
-              }
+              // {
+              //   id: 'Mercurio',
+              //   Title: 'Mercurio', 
+              //   Img: "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
+              //   Paragrafh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, obcaecati velit consequatur doloremque qui asperiores distinctio culpa ducimus recusandae. Facilis aliquid sequi amet. Pariatur eveniet molestiae exercitationem voluptate eligendi fuga.",
+              //   Link: "https://pt.wikipedia.org/wiki/Marte_(planeta)"
+              // }
             ]
         }
     }
+
+
+    componentDidMount() {
+      getPlanets().then(data => {
+        this.setState(state => ({
+          Planets: data['planets']
+        }))
+      })
+    }
+
 
     addPlanets = () => {
       const Title = prompt('Name of planet')
