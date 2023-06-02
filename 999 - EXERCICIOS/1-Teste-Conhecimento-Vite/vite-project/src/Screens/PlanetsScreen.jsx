289bbbc8bf631b30f4b88f8aef29795e3c785ffe
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-06-02 14:47:53 -0300

    adicionando a biblioteca react-router-dom

---
+++ 999 - EXERCICIOS/1-Teste-Conhecimento-Vite/vite-project/src/Screens/PlanetsScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Planetas from "../components/Planetas";
+
+const PlanetsScreen = () => {
+    return (
+        <Planetas />
+    )
+}
+
+export default PlanetsScreen;
