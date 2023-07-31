481a5bb1aaa255a4312144ef0d4dc35e60c0fbbe
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-07-31 19:06:54 -0300

    instalando o Bootstrap no projeto
    Primeira fez que eu baixo o Bootstrap como uma pasta

--- 999 - EXERCICIOS/2-Perguntas-Respostas/index.js
+++ 999 - EXERCICIOS/2-Perguntas-Respostas/index.js
@@ -1,38 +1,18 @@
 const express = require('express');
 const appExpress = express();
 
-
 // Dizendo para o express utilizar o "ejs" do view engine
 appExpress.set('view engine', 'ejs')
 
-appExpress.get('/:nome?', (req, res) => {
-    
-    const nome = req.params.nome
-    const produtos = [
-        {nome: 'TV', preco: 3000},
-        {nome: 'Notebook', preco: 2300},
-        {nome: 'tablet', preco: 1200},
-        {nome: 'sofa', preco: 2700}
-    ]
+// Inserindo arquivos estaticos como CSS e JS no express
+appExpress.use(express.static('public'))
 
+
+appExpress.get('/', (req, res) => {
     res.render('index', {
-        nome: nome,
-        idade: 19,
-        curso: "Funçoes Node.js",
-        produtos: produtos
+        
     })
 });
-
-
-appExpress.get('/perfil', (req, res) => {
-    res.render('principal/perfil', {
-        usuario: "Ramon",
-        idade: 19,
-        proficion: "Desenvolvedor Web",
-        empregado: false
-    })
-});
-
 
 appExpress.listen('4000', () => {
     console.log('Servidor rodando...');
