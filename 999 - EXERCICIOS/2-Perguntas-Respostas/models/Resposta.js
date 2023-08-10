842b277cf3e9798d0383742cbf915830227dbd80
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-08-10 11:20:46 -0300

    apagando projeto

--- 999 - EXERCICIOS/2-Perguntas-Respostas/models/Resposta.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Resposta = mongoose.model('Resposta', {
-    data: {type: String, required: true},
-    texto: {type: String, required: true},
-    perguntaId: {type: String, required: true}
-})
-
-module.exports = Resposta
--- 999 - EXERCICIOS/2-Perguntas-Respostas/models/pergunta.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const Pergunta = mongoose.model("Pergunta", {
-    title: String,
-    descrition: String
-})
-
-module.exports = Pergunta
