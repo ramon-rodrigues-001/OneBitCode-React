603640e8c09476e6b653d4e850a3f9203fcd60c9
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-08-31 12:11:08 -0300

    Header quase pronto, isso vai demorar.

--- 999 - EXERCICIOS/newPortfolio/src/layout/1-header/header.jsx
+++ 999 - EXERCICIOS/newPortfolio/src/layout/1-header/header.jsx
@@ -1,51 +1,57 @@
+import styles from "./header.module.scss"
+
 
 export default function Header() {
     return (
-        <header id="header-home">
-            <h1 id="logo"><span className="span-azulMarinho">&lt; / D</span>EV &gt;</h1>
+        <header id={styles.headerHome}>
+            <h1 id={styles.logo}>
+                {/* detalhe */}
+                <span className={styles.spanAzulMarinho}>&lt; / D</span>EV &gt;
+            </h1>
 
             {/* Navbar para tela quandes */}
-            <nav id="navbar">
+            <nav id={styles.navbar}>
                 <a href="#sobre-mim">
-                    <span className="aspas-navbar">&lt; </span>
+                    <span className={styles.aspasNavbar}>&lt; </span>
                         SOBRE MIM
-                    <span className="aspas-navbar"> &gt;</span>
+                    <span className={styles.aspasNavbar}> &gt;</span>
                 </a>
                 <a href="#habilidades">
-                    <span className="aspas-navbar">&lt; </span>
+                    <span className={styles.aspasNavbar}>&lt; </span>
                         HABILIDADES
-                    <span className="aspas-navbar"> &gt;</span>
+                    <span className={styles.aspasNavbar}> &gt;</span>
                 </a>
                 <a href="#ancora-projetos">
-                    <span className="aspas-navbar">&lt; </span>
+                    <span className={styles.aspasNavbar}>&lt; </span>
                         PROJETOS
-                    <span className="aspas-navbar"> &gt;</span>
+                    <span className={styles.aspasNavbar}> &gt;</span>
                 </a>
                 <a href="#contatos">
-                    <span className="aspas-navbar">&lt; </span>
+                    <span className={styles.aspasNavbar}>&lt; </span>
                         CONTATOS
-                    <span className="aspas-navbar"> &gt;</span>
+                    <span className={styles.aspasNavbar}> &gt;</span>
                 </a>
             </nav>
             {/* / Navbar para tela quandes */}
             
             {/* Navbar menu hamburguer tela peguena até 650px */}
-            <input type="checkbox" name="check-menu" id="check-menu" />
-            <menu id="hamburguer">
+            {/* Detalhe */}
+            <input type="checkbox" name="check-menu" id={styles.checkMenu} />
+            <menu id={styles.hamburguer}>
                 <label for="check-menu">
-                    <div id="menu" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <div className="tracos" id="traco-1"></div>
-                        <div className="tracos" id="traco-2"></div>
-                        <div className="tracos" id="traco-3"></div>
+                    <div id={styles.menu} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <div className={styles.tracos} id={styles.traco-1}></div>
+                        <div className={styles.tracos} id={styles.traco-2}></div>
+                        <div className={styles.tracos} id={styles.traco-3}></div>
                     </div>
                 </label>
                 
-                <div className="container-fluid">
-                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" data-bs-backdrop="false">
+                <div className="container-fluid" id={styles.containerFluid}>
+                    <div className="offcanvas offcanvas-end" tabindex="-1" id={styles.offcanvasNavbar} aria-labelledby="offcanvasNavbarLabel" data-bs-backdrop="false">
                         <div className="offcanvas-header mx-5">
                             <h5 className="offcanvas-title" id="offcanvasNavbarLabel">&lt; DEV / &gt;</h5>
                         </div>
-                        <div className="offcanvas-body">
+                        <div className="offcanvas-body" id={styles.offcanvasBody}>
                             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                                 <li className="nav-item">
                                     <a className="btn btn-outline-light btnPersonalizado itens_menu" href="#sobre-mim">Sobre Mim</a>
@@ -77,8 +83,8 @@ export default function Header() {
             
 
             <div>
-                <a className="btn btn-outline-info btnPersonalizado me-2" href="https://github.com/ramon-rodrigues-001/" target="_blank" id="butao-github"><i className="bi bi-github"></i></a>
-                <a className="btn btn-outline-info btnPersonalizado me-2" href="https://www.linkedin.com/in/ramon-rodrigues-515a9b244/" target="_blank" id="butao-github"><i className="bi bi-linkedin"></i></a>
+                <a className="btn btn-outline-info btnPersonalizado me-2" href="https://github.com/ramon-rodrigues-001/" target="_blank" id={styles.butaoGithub}><i className="bi bi-github"></i></a>
+                <a className="btn btn-outline-info btnPersonalizado me-2" href="https://www.linkedin.com/in/ramon-rodrigues-515a9b244/" target="_blank" id={styles.butaoGithub}><i className="bi bi-linkedin"></i></a>
             </div>
         </header>
     )
