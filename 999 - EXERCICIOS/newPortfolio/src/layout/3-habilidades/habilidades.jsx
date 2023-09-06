15e1e097e29339de259dfee1db0808532df2020b
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-09-06 11:25:32 -0300

    section de habilidades

--- 999 - EXERCICIOS/newPortfolio/src/layout/3-habilidades/habilidades.jsx
+++ 999 - EXERCICIOS/newPortfolio/src/layout/3-habilidades/habilidades.jsx
@@ -12,49 +12,49 @@ export default function Habilidades() {
                         <div className={styles.cards} data-aos="zoom-in" id="hab-1">
                             <a href="#habilidades" >
                                 <img src="public/icon-habilits/html5-plain.svg" className={styles.iconHabilits} alt="icon_HTML_5" />
-                                <h3 className={styles.title_ability}>HTML5</h3>
+                                <h3 className={styles.title_ability}></h3>
                                 <p className="h5 px-2">Avançado</p>
                             </a>
                         </div>
                         <div className={styles.cards} data-aos="zoom-in" id="hab-2">
                             <a href="#habilidades">
                                 <img src="public/icon-habilits/css3-plain.svg" className={styles.iconHabilits} alt="icon_CSS_3" />
-                                <h3 className={styles.title_ability}>CSS</h3>
+                                <h3 className={styles.title_ability}></h3>
                                 <p className="h5 px-2">Avançado</p>
                             </a>
                         </div>
                         <div className={styles.cards} data-aos="zoom-in" id="hab-3">
                             <a href="#habilidades">
                                 <img src="public/icon-habilits/javascript-plain.svg" className={styles.iconHabilits} alt="icon_JS" />
-                                <h3 className="title_ability">JAVASCRIPT</h3>
+                                <h3 className="title_ability"></h3>
                                 <p className="h5 px-2">Avançado</p>
                             </a>
                         </div>
                         <div className={styles.cards} data-aos="zoom-in" id="hab-4">
                             <a href="#habilidades">
-                                <img src="public/icone-habilidades/sass.png" className={styles.iconHabilits} alt="icon_SASS" />
-                                <h3 className={styles.title_ability}>SASS</h3>
+                                <img src="/icon-habilits/sass-original.svg" className={styles.iconHabilits} alt="icon_SASS" />
+                                <h3 className={styles.title_ability}></h3>
                                 <p className="h5 px-2">Avançado</p>
                             </a>
                         </div>
                         <div className={styles.cards} data-aos="zoom-in" id="hab-5">
                             <a href="#habilidades">
                                 <img src="public/icon-habilits/bootstrap-plain.svg" className={styles.iconHabilits} alt="icon_BOOTSTRAP" />
-                                <h3 className={styles.title_ability}>BOOTSTRAP</h3>
+                                <h3 className={styles.title_ability}></h3>
                                 <p className="h5 px-2">Basico</p>
                             </a>
                         </div>
                         <div className={styles.cards} data-aos="zoom-in" id="hab-6">
                             <a href="#habilidades">
                                 <img src="public/icone-habilidades/react.png" className={styles.iconHabilits} alt="icon_REACT" />
-                                <h3 className={styles.title_ability}>REACT</h3>
+                                <h3 className={styles.title_ability}></h3>
                                 <p className="h5 px-2">Basico</p>
                             </a>
                         </div>
                         <div className={styles.cards} data-aos="zoom-in" id="hab-7">
                             <a href="#habilidades">
                                 <img src="public/icon-habilits/nodejs-plain.svg" className={styles.iconHabilits} alt="icon_NodeJs" />
-                                <h3 className={styles.title_ability}>NodeJs</h3>
+                                <h3 className={styles.title_ability}></h3>
                                 <p className="h5 px-2">Basico</p>
                             </a>
                         </div>
@@ -62,17 +62,17 @@ export default function Habilidades() {
                         <div className={styles.cards}  data-aos="zoom-in"  id="hab-8">
                             <a href="#habilidades">
                                 <img src="public/icon-habilits/mongodb-plain.svg" className={styles.iconHabilits} alt="icon_mongoDB" />
-                                <h3 className={styles.title_ability}>MongoDB</h3>
+                                <h3 className={styles.title_ability}></h3>
                                 <p className="h5 px-2">Basico</p>
                             </a>
                         </div>
-                        <div className={styles.cards} data-aos="zoom-in" id="hab-9">
+                        {/* <div className={styles.cards} data-aos="zoom-in" id="hab-9">
                             <a href="#habilidades">
                                 <img src="public/icon-habilits/nodejs-plain.svg" className={styles.iconHabilits} alt="icon_TYPESCRIPT" />
-                                <h3 className={styles.title_ability}>Express</h3>
+                                <h3 className={styles.title_ability}></h3>
                                 <p className="h5 px-2">Basico</p>
                             </a>
-                        </div>
+                        </div> */}
 
                         <label for="mais-habilidades">
                             <a className="btn btn-outline-info btnPersonalizado" id="btn-mais-habilidades">MOSTRAR MAIS</a>
