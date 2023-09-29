5d9e52c4b26d96426fa148782c2976429f78b2a4
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-09-29 17:32:20 -0300

    iniciando projeto comunidade

---
+++ 999 - EXERCICIOS/2-Comunidade-Local/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Logo</h1>
+        <ul>
+          <li>Opition</li>
+          <li>Opition</li>
+          <li>Opition</li>
+          <li>Opition</li>
+        </ul>
+        <div>
+          <button>Login</button>
+          <button>Cadastro</button>
+        </div>
+      </header>
+      <div className="home">
+        <img src="/background-home.jpg" alt="background-hoe" />
+      </div>
+    </div>
+  )
+}
+
+export default App
