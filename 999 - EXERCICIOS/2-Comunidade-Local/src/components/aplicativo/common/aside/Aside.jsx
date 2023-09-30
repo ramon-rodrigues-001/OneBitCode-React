68c82d172d3a199e5e94225eed8b3607151ec87e
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-09-30 10:46:33 -0300

    Amigos

--- 999 - EXERCICIOS/2-Comunidade-Local/src/components/aplicativo/common/aside/Aside.jsx
+++ 999 - EXERCICIOS/2-Comunidade-Local/src/components/aplicativo/common/aside/Aside.jsx
@@ -3,11 +3,51 @@ import styles from "./Aside.module.scss"
 function Aside() {
   return (
     <div className={styles.aside}>
-        <h3>Amigos</h3>
+        <h3>Seus Amigos</h3>
 
         <div className={styles.containerAmigos}>
             <div className={styles.cardsAmigos}>
               Ramon
+              <div>
+                <span className={styles.span}></span>
+                <i class="bi bi-pencil-fill"></i>
+              </div>
+            </div>
+
+            <div className={styles.cardsAmigos}>
+              Davi
+              <div>
+                <span className={styles.span}></span>
+                <i class="bi bi-pencil-fill"></i>
+              </div>
+            </div>
+
+            <div className={styles.cardsAmigos}>
+              Maria
+              <div>
+                <span className={styles.span}></span>
+                <i class="bi bi-pencil-fill"></i>
+              </div>
+            </div>
+
+            <div className={styles.cardsAmigos}>
+              Lucas
+              <div>
+                <span className={styles.span}></span>
+                <i class="bi bi-pencil-fill"></i>
+              </div>
+            </div>
+
+            <div className={styles.cardsAmigos}>
+              Ray
+              <div>
+                <span className={styles.span}></span>
+                <i class="bi bi-pencil-fill"></i>
+              </div>
+            </div>
+
+            <div className={styles.cardsAmigos}>
+              Lorem ipsum dolor
               <div>
                 <span className={styles.span}></span>
                 <i class="bi bi-pencil-fill"></i>
