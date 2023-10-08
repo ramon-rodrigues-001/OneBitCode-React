45b149e6177a6f484dba0933fea88e458f17aad3
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-10-08 20:41:24 -0300

    fim do dia

--- 999 - EXERCICIOS/2-Comunidade-Local/src/components/aplicativo/pages/bate_papo/BatePapo.jsx
+++ 999 - EXERCICIOS/2-Comunidade-Local/src/components/aplicativo/pages/bate_papo/BatePapo.jsx
@@ -9,8 +9,14 @@ function BatePapo() {
         <div className={styles.cardDeGrupos}></div>
       </div>
       <div className={styles.containerCardsNovosGrupos}>
-        <div className={styles.cardNovoGrupo}></div>
-        <div className={styles.cardNovoGrupo}></div>
+        <div className={styles.cardNovoGrupo}>
+          <i class="bi bi-plus-lg"></i>
+          <p>Achar grupo</p>
+        </div>
+        <div className={styles.cardNovoGrupo}>
+          <i class="bi bi-plus-square-dotted"></i>
+          <p>Criar novo gropo</p>
+        </div>
       </div>
     </section>
   )
