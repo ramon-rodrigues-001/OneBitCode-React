c22e0b3bb5f6b58e4c46acd108a2b150d08ec81a
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-10-19 21:12:47 -0300

    Fim do dia

--- 999 - EXERCICIOS/2-Comunidade-Local/frontEnd/src/components/aplicativo/common/header/Header.jsx
+++ 999 - EXERCICIOS/2-Comunidade-Local/frontEnd/src/components/aplicativo/common/header/Header.jsx
@@ -53,6 +53,14 @@ function Header() {
                   <i class="bi bi-x"></i>
                 </button>
               </div>
+              <div className={styles.bodyMenu}>
+                <div className={styles.navIconesMenu}>
+                  <a href="/"><i class="bi bi-cup-hot-fill"></i></a>
+                  <a href="/batePapo"><i class="bi bi-people-fill"></i></a>
+                  <a href="/noticias"><i class="bi bi-newspaper"></i></a>
+                  <a href="/perfil"><i class="bi bi-person-fill"></i></a>
+                </div>
+              </div>
             </div>
           )}
         </div>
